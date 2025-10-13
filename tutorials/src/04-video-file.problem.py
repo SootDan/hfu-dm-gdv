0b4bb97673a032c0d0597bb5703d53480bb216c9
SootDan <124447854+SootDan@users.noreply.github.com>
2025-10-13 20:56:05 +0200

    feat: ex4

--- tutorials/src/04-video-file.problem.py
+++ tutorials/src/04-video-file.problem.py
@@ -6,5 +6,30 @@
 import numpy as np
 import cv2
 
-# TODO Just copy the code from exercise 3 and modify it to load a video file instead of the camera.
-# Use the file "tutorials/data/videos/hello_UH.m4v" as input for the 'VideoCapture' function.
+cap: cv2.VideoCapture = cv2.VideoCapture("tutorials/data/videos/hello_UH.m4v")
+
+width: float = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height: float = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+codec: float = cap.get(cv2.CAP_PROP_CODEC_PIXEL_FORMAT)
+print(f"Width: {width}\nHeight: {height}\nCodec: {codec}")
+
+cv2.namedWindow("Video image", cv2.WINDOW_GUI_NORMAL)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    key = cv2.waitKey(20)
+
+    img = np.zeros_like(frame)
+    tile = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    img[:tile.shape[0], :tile.shape[1]] = tile
+    img[tile.shape[0]:, :tile.shape[1]] = cv2.flip(tile, 0)
+    img[:tile.shape[0], tile.shape[1]:] = cv2.flip(tile, 1)
+    img[tile.shape[0]:, tile.shape[1]:] = cv2.flip(tile, -1)
+    cv2.imshow("Video image", img)
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
