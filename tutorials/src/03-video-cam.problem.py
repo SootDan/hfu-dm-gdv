1276af686c55536cfe29d719ad122e5644c446fb
Daniel B. Georg <124447854+SootDan@users.noreply.github.com>
2025-10-13 17:15:00 +0200

    feat: ex3

--- tutorials/src/03-video-cam.problem.py
+++ tutorials/src/03-video-cam.problem.py
@@ -14,24 +14,22 @@ codec: float = cap.get(cv2.CAP_PROP_CODEC_PIXEL_FORMAT)
 print(f"Width: {width}\nHeight: {height}\nCodec: {codec}")
 
 cv2.namedWindow("Video image", cv2.WINDOW_GUI_NORMAL)
-#cv2.imshow("Image", cap)
-#cv2.waitKey(0)
-# TODO Start a loop to continuously read frames from the camera using 'while True:'.
+
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    
-# TODO (In loop, if successful) (Skip this step for now and implement the display first in order 
-# to see the camera image.)
-# Create four flipped tiles of the image by first creating an empty image 
-# with 'np.zeros'. Then resize the frame to half size using 'cv2.resize' with fx=0.5 and fy=0.5.
-# Finally, fill in the four quadrants of the empty image with the original and flipped images
-# using 'cv2.flip' with the appropriate flip codes (check the documentation).
+    key = cv2.waitKey(20)
 
-# TODO (In loop, if successful) Display the image.
+    img = np.zeros_like(frame)
+    tile = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    img[:tile.shape[0], :tile.shape[1]] = tile
+    img[tile.shape[0]:, :tile.shape[1]] = cv2.flip(tile, 0)
+    img[:tile.shape[0], tile.shape[1]:] = cv2.flip(tile, 1)
+    img[tile.shape[0]:, tile.shape[1]:] = cv2.flip(tile, -1)
+    cv2.imshow("Video image", img)
+    if key == ord('q'):
+        break
 
-# TODO (In loop) Check if 'q' is pressed by comparing the return value of 'waitKey' with the 'ord'
-# of the letter "q". Use simply 'break' to exit the loop.
-
-# TODO Release the video capture object with 'release' and close the window with 'destroyAllWindows'.
+cap.release()
+cv2.destroyAllWindows()
